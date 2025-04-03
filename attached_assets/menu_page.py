d833dc6002aac94ff198faf8173c9a279b1aa74b
DeviDeviDeviKanumilli <166880231+DeviDeviDeviKanumilli@users.noreply.github.com>
2025-04-03 14:14:45 -0400

    Delete attached_assets directory

--- attached_assets/menu_page.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from info import sample_dishes
-
-def menu_page():
-    st.header("Our Full Menu")
-    for dish in sample_dishes:
-        st.subheader(dish["name"])
-        st.markdown(f"<em>Origin:</em> {dish['origin']}", unsafe_allow_html=True)
-        st.markdown("---")
